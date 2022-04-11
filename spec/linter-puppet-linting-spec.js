10972e1855e236ef3ea59b6251506809df419b84
Matt Schuchard <grapheneowns@gmail.com>
2022-04-11 11:08:23 -0400

    begin fixing and updating unit tests

--- spec/linter-puppet-linting-spec.js
+++ spec/linter-puppet-linting-spec.js
@@ -1,18 +1,17 @@
 'use babel';
 
 // eslint-disable-next-line no-unused-vars
-import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 import * as path from 'path';
 
 const cleanPath = path.join(__dirname, 'fixtures', 'test_clean.pp');
 const errorsPath = path.join(__dirname, 'fixtures', 'test_errors.pp');
 
 describe('The puppet-lint provider for Linter', () => {
-  const { lint } = require('../lib/main.js').provideLinter();
+  const { lint } = require(path.join(__dirname, '../lib/main.js')).provideLinter();
 
   beforeEach(async () => {
     atom.workspace.destroyActivePaneItem();
-    await atom.packages.activatePackage('linter-puppet-lint');
+    await atom.packages.activatePackage('linter-puppet-linting');
     await atom.packages.activatePackage('language-puppet');
   });
 
@@ -27,11 +26,16 @@ describe('The puppet-lint provider for Linter', () => {
     const editor = await atom.workspace.open(errorsPath);
     const messages = await lint(editor);
 
-    expect(messages.length).toBe(2);
-
-    expect(messages[0].html).not.toBeDefined();
-    expect(messages[0].location.file).toBe(errorsPath);
-    expect(messages[1].html).not.toBeDefined();
-    expect(messages[1].location.file).toBe(errorsPath);
+    expect(messages.length).toEqual(2);
+    expect(messages[0].severity).toBeDefined();
+    expect(messages[0].excerpt).toBeDefined();
+    expect(messages[0].location.file).toBeDefined();
+    expect(messages[0].location.file).toEqual(errorsPath);
+    expect(messages[0].location.position).toBeDefined();
+    expect(messages[1].severity).toBeDefined();
+    expect(messages[1].excerpt).toBeDefined();
+    expect(messages[1].location.file).toBeDefined();
+    expect(messages[1].location.file).toEqual(errorsPath);
+    expect(messages[1].location.position).toBeDefined();
   });
 });
